a7a65e1e179abec4566205280964c67c2cdc57e5
jrMM93 <jeremyatn@gmail.com>
2020-06-02 12:36:47 +0200

    added users/id route

---
+++ src/Controller/Api/V1/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/api/v1/public/users", name="api_v1_users_")
+ */
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("/{id}", name="read", methods={"GET"}, requirements={"id": "\d+"})
+     */
+    public function read(User $user, SerializerInterface $serializer)
+    {
+        $arrayUser = $serializer->normalize($user, null, ['groups' => 'user_read']);
+
+        return $this->json($arrayUser);
+    }
+}
